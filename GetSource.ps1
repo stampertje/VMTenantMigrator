b9c4148c82be968f76e31f60e196c8d7be27c4fd
Nico van Diemen <nicovan@microsoft.com>
2022-08-16 18:37:30 +0200

    Fixed bugs

--- GetSource.ps1
+++ GetSource.ps1
@@ -1,4 +1,5 @@
-#Requires -module Az.Compute, Az.Accounts, Az.Storage
+#Requires -module Az.Compute, Az.Accounts, Az.Storage, Az.Resources
+# Stole code from here https://www.whatsupgold.com/blog/how-to-rename-an-azure-vm-using-powershell-a-step-by-step-guide
 
 [CmdletBinding()]
 param (
@@ -18,14 +19,14 @@ param (
     $SourceSubscription,
 
     # The name of the target storage account
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$false)]
     [string]
     $TargetStorageAccountName,
 
-    # SAS key for target storage account
-    [Parameter(Mandatory=$true)]
+    # Connectionstring for target storage account
+    [Parameter(Mandatory=$false)]
     [string]
-    $TargetStorageAccountSAS,
+    $ConnectionString,
 
     # If name is specified only single VM will be migrated. Expecting the VMName
     [Parameter()]
@@ -34,57 +35,62 @@ param (
 
     # Defines if automation credentials should be used or 
     [Parameter()]
-    $InAutomationAccount
+    [boolean]
+    $InAutomationAccount=$false
 )
 
 if (!(test-path $tempdir)){mkdir $tempdir}
 
-If ($InAutomationAccount)
+if ((get-azcontext).subscription.id -ne $SourceSubscription)
 {
-
-  $connectionName = "AzureRunAsConnection";
- 
-  try
+  If ($InAutomationAccount -eq $true)
   {
-      # Get the connection "AzureRunAsConnection "
-      $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName        
+
+    $connectionName = "AzureRunAsConnection";
   
-      "Logging in to Azure..."
-      Add-AzAccount `
-          -ServicePrincipal `
-          -TenantId $servicePrincipalConnection.TenantId `
-          -ApplicationId $servicePrincipalConnection.ApplicationId `
-          -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
-  }
-  catch {
-  
-      if (!$servicePrincipalConnection)
-      {
-          $ErrorMessage = "Connection $connectionName not found."
-          throw $ErrorMessage
-      } else{
-          Write-Error -Message $_.Exception
-          throw $_.Exception
-      }
+    try
+    {
+        # Get the connection "AzureRunAsConnection "
+        $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName        
+    
+        "Logging in to Azure..."
+        Add-AzAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+    }
+    catch {
+    
+        if (!$servicePrincipalConnection)
+        {
+            $ErrorMessage = "Connection $connectionName not found."
+            throw $ErrorMessage
+        } else{
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+
+  } else {
+    
+    Login-AzAccount -Tenant $SourceTenant
+
   }
 
-} else {
-  
-  Login-AzAccount -Tenant $SourceTenant
-
+  Select-AzSubscription -SubscriptionId $SourceSubscription
 }
 
-
-Select-AzSubscription -SubscriptionId $SourceSubscription
-
 #connect to storage account
-$storageContext = New-AzureStorageContext -StorageAccountName $TargetStorageAccountName -SasToken $TargetStorageAccountSAS
+#$storageContext = New-AzStorageContext -StorageAccountName $TargetStorageAccountName -SasToken $TargetStorageAccountSAS
+if($connectionstring){$storageContext = New-AzStorageContext -ConnectionString $ConnectionString}
+if($UseCurrentAccount){$StorageContext = New-AzStorageContext -StorageAccountName $TargetStorageAccountName -UseConnectedAccount}
 $ContainerName = "vmbackup"
-New-AzStorageContainer -Name $ContainerName -Context $storageContext -Permission Blob 
+New-AzStorageContainer -Name $ContainerName -Context $storageContext -Permission Blob
 
 if ($MigrateSingleVM)
 {
-  $VMtoMigrate = Get-AzResource $MigrateSingleVM | Where-Object {$_.ResourceType -like "Microsoft.Compute/virtualMachines"}
+  $VMtoMigrate = Get-AzVm -name $MigrateSingleVM
 } else {
   $VMtoMigrate = Get-AzVm
 }
@@ -92,9 +98,9 @@ if ($MigrateSingleVM)
 Foreach ($vm in $VMtoMigrate)
 {
   $vmname = $vm.Name
-  Get-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vmName | Export-Clixml C:\temp\$vmname.xml -Depth 5
-  # copy xml to storage account
+  Get-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vmName | Export-Clixml $tempdir\$vmname.xml -Depth 5
 
+  # copy xml to storage account
   Set-AzStorageBlobContent `
     -Context $storageContext `
     -Container $ContainerName `
@@ -103,7 +109,11 @@ Foreach ($vm in $VMtoMigrate)
     -force
 
   # shutdown vm if running
-  Stop-VM $vm -Force
+  $vmstatus = $vm.status
+  if($vmstatus -ieq "Running")
+  {
+    Stop-AzVM -name $vm.name -ResourceGroupName $vm.ResourceGroupName -Force
+  }
 
   # clone disk to storage account
   $osdisk = get-azdisk -ResourceGroupName $vm.ResourceGroupName -name $vm.StorageProfile.osdisk.name
@@ -117,5 +127,9 @@ Foreach ($vm in $VMtoMigrate)
     Start-AzStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestinationContainer $ContainerName -DestinationBlob $disk.name -DestinationContext $storageContext
   }
 
+  If ($vmstatus -ieq "Running")
+  {
+    Start-AzVm -Name $vm.name -ResourceGroupName $vm.ResourceGroupName
+  }
 }
 
